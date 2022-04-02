0992288ac5b250d14eb96053ae221f77790b9cf4
isanjit3 <isanjit3@gmail.com>
2022-04-02 02:37:04 -0400

    add-user

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const bodyParser = require("body-parser");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient } = require("mongodb");
 const uri = "mongodb+srv://peer-seeder-admin:CloudSystems1@peer-seeder.dinup.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri);
 const PORT = 3000 || process.env.PORT;
@@ -32,12 +32,32 @@ app.get("/profile", async (req, res) => {
   res.render("profile");
 });
 
+app.get("/add-user", async (req, res) => {
+  res.render("temp/add-user");
+});
+
+app.get("/add-tree", async (req, res) => {
+  res.render("temp/add-tree");
+});
+
 /*
   API CALLS
 */
+// USER CALLS
 // add new user to database
 app.post("/addUser", async (req, res) => {
-  res.status(200);
+  pos = getUserPosition()
+
+  user = {
+    username: req.body.username,
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    latitude: pos[lat],
+    longitude: post[long],
+  }
+
+  console.log(user)
+  res.status(200)
 });
 
 // update existing user in database
@@ -46,6 +66,11 @@ app.post("/updateUser", async (req, res) => {
 });
 
 app.delete("/deleteUser", async (req, res) => {
+  res.status(200);
+});
+
+// TREE CALLS
+app.post("/addTree", async (req, res) => {
   res.status(200);
 });
 
@@ -61,6 +86,22 @@ async function main() {
     console.error(e);
   } finally {
     await client.close();
+  }
+}
+
+async function getUserPosition() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      coords = {
+        lat: position.coords.latitude,
+        long: position.coords.longitude
+      }
+      console.log(coords)
+      return coords;
+    }, 
+    (error) => {
+      console.log(error.message)
+    });
   }
 }
 
